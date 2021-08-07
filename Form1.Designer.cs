0b3a4f9fc04045e603dee8cafc3ba17094576c7e
Elvis <Elvis@DESKTOP-7IJJ5TI>
2021-08-07 15:08:39 +0100

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿
+namespace Ejercio01
+{
+    partial class frmPagoEmpleado
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEmpleado = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblComision = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.cboImporte = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblSueldo = new System.Windows.Forms.Label();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lvEmpleados = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tHora = new System.Windows.Forms.Timer(this.components);
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Control de Pagos de Empleados";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblHora);
+            this.groupBox1.Controls.Add(this.lblFecha);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(510, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 64);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fecha y Hora";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(52, 36);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(40, 13);
+            this.lblHora.TabIndex = 4;
+            this.lblHora.Text = "lblHora";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(52, 16);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(52, 13);
+            this.lblFecha.TabIndex = 3;
+            this.lblFecha.Text = "lblFechas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hora:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(217, 16);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Nombre y Apellidos del  empleado";
+            // 
+            // txtEmpleado
+            // 
+            this.txtEmpleado.Location = new System.Drawing.Point(6, 45);
+            this.txtEmpleado.Name = "txtEmpleado";
+            this.txtEmpleado.Size = new System.Drawing.Size(257, 22);
+            this.txtEmpleado.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSalir);
+            this.groupBox2.Controls.Add(this.lblComision);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.btnProcesar);
+            this.groupBox2.Controls.Add(this.cboImporte);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.lblSueldo);
+            this.groupBox2.Controls.Add(this.cboCategoria);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtEmpleado);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 79);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(756, 162);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos del Empleado";
+            // 
+            // lblComision
+            // 
+            this.lblComision.AutoSize = true;
+            this.lblComision.Location = new System.Drawing.Point(436, 92);
+            this.lblComision.Name = "lblComision";
+            this.lblComision.Size = new System.Drawing.Size(78, 16);
+            this.lblComision.TabIndex = 13;
+            this.lblComision.Text = "lblComision";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(435, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Comision %";
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(536, 38);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(103, 36);
+            this.btnProcesar.TabIndex = 11;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // cboImporte
+            // 
+            this.cboImporte.FormattingEnabled = true;
+            this.cboImporte.Items.AddRange(new object[] {
+            "≥9000",
+            "≥ 6000 y <9000",
+            "≥ 3000 y < 6000",
+            "<3000"});
+            this.cboImporte.Location = new System.Drawing.Point(292, 89);
+            this.cboImporte.Name = "cboImporte";
+            this.cboImporte.Size = new System.Drawing.Size(121, 24);
+            this.cboImporte.TabIndex = 10;
+            this.cboImporte.Text = "(Selec. Importe)";
+            this.cboImporte.SelectedIndexChanged += new System.EventHandler(this.cboImporte_SelectedIndexChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(289, 70);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(107, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Importe Vendido";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(167, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Sueldo Basico";
+            // 
+            // lblSueldo
+            // 
+            this.lblSueldo.AutoSize = true;
+            this.lblSueldo.Location = new System.Drawing.Point(167, 97);
+            this.lblSueldo.Name = "lblSueldo";
+            this.lblSueldo.Size = new System.Drawing.Size(65, 16);
+            this.lblSueldo.TabIndex = 7;
+            this.lblSueldo.Text = "lblSueldo";
+            // 
+            // cboCategoria
+            // 
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Items.AddRange(new object[] {
+            "E1",
+            "E2",
+            "E3"});
+            this.cboCategoria.Location = new System.Drawing.Point(6, 89);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(121, 24);
+            this.cboCategoria.TabIndex = 6;
+            this.cboCategoria.Text = "(Selec. Categoria)";
+            this.cboCategoria.SelectedIndexChanged += new System.EventHandler(this.cboCategoria_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 16);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Categoria";
+            // 
+            // lvEmpleados
+            // 
+            this.lvEmpleados.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvEmpleados.GridLines = true;
+            this.lvEmpleados.HideSelection = false;
+            this.lvEmpleados.Location = new System.Drawing.Point(6, 247);
+            this.lvEmpleados.Name = "lvEmpleados";
+            this.lvEmpleados.Size = new System.Drawing.Size(646, 179);
+            this.lvEmpleados.TabIndex = 6;
+            this.lvEmpleados.UseCompatibleStateImageBehavior = false;
+            this.lvEmpleados.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Empleado";
+            this.columnHeader1.Width = 192;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Categoria";
+            this.columnHeader2.Width = 68;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Sueldo Basico";
+            this.columnHeader3.Width = 84;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Comision";
+            this.columnHeader4.Width = 59;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Sueldo Bruto";
+            this.columnHeader5.Width = 80;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Descuento";
+            this.columnHeader6.Width = 74;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Sueldo Neto";
+            this.columnHeader7.Width = 77;
+            // 
+            // tHora
+            // 
+            this.tHora.Tick += new System.EventHandler(this.tHora_Tick);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(536, 87);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(103, 36);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 231);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Estadisticas ";
+            // 
+            // frmPagoEmpleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 517);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lvEmpleados);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPagoEmpleado";
+            this.Text = "Pago de Empleados";
+            this.Load += new System.EventHandler(this.frmPagoEmpleado_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEmpleado;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.ComboBox cboImporte;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListView lvEmpleados;
+        private System.Windows.Forms.Timer tHora;
+        private System.Windows.Forms.Label lblComision;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercio01
+{
+    public partial class frmPagoEmpleado : Form
+    {
+        //Declarando variables goblales
+        double sueldo;
+        double comision;
+        public frmPagoEmpleado()
+        {
+            InitializeComponent();
+            tHora.Enabled = true;
+        }
+
+        private void tHora_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void frmPagoEmpleado_Load(object sender, EventArgs e)
+        {
+            //Mostrando la fecha actual
+            lblFecha.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void cboCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string categoria = cboCategoria.Text;
+
+            switch (categoria)
+            {
+                case "E1": sueldo = 2500; break;
+                case "E2": sueldo = 2250; break;
+                case "E3": sueldo = 2000; break;
+            }
+            lblSueldo.Text = sueldo.ToString("C");
+        }
+
+        private void cboImporte_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string importe = cboImporte.Text;
+            switch (importe)
+            {
+                case "≥9000": comision = sueldo * 11 /100; break;
+                case "≥ 6000 y <9000":  comision = sueldo * 9/100; break;
+                case "≥ 3000 y < 6000": comision = sueldo * 7/100; break;
+                case "<3000": comision = sueldo * 5/100; break;
+            }
+            lblComision.Text = comision.ToString("C");
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            //Capturando los datos
+            string empleado = txtEmpleado.Text;
+            string categoria = cboCategoria.Text;
+            string sueldo1 = lblSueldo.Text;
+            string comision1 = lblComision.Text;
+
+            //calculando sueldo bruto
+            double sueldoBruto = sueldo + comision;
+            //calculando descuento
+            double descuento = sueldoBruto * 15 / 100;
+            //calculando sueldo net0
+            double sueldoneto = sueldoBruto - descuento;
+
+            //Impresion de planilla
+            ListViewItem fila = new ListViewItem(empleado);
+            fila.SubItems.Add(categoria);
+            fila.SubItems.Add(sueldo1.ToString());
+            fila.SubItems.Add(comision1.ToString());
+            fila.SubItems.Add(sueldoBruto.ToString("C"));
+            fila.SubItems.Add(descuento.ToString("C"));
+            fila.SubItems.Add(sueldoneto.ToString("C"));
+
+            lvEmpleados.Items.Add(fila);
+
+            //Limpiando los controles
+            txtEmpleado.Clear();
+            cboCategoria.SelectedIndex = -1;
+            lblSueldo.Text = (0).ToString("C");
+            txtEmpleado.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Esta seguro de salir?", "Pago de Empleados", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (r == DialogResult.Yes) this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
